a78cc82de46a4a7c5b502d7998c9bd301ef4bd86
Jason <Jason>
2016-10-11 17:21:37 -0600

    add js file and final changes

---
+++ js/glow.js
@@ -0,0 +1,34 @@
+$(function () {
+    var $cultLogo = $('#logo');
+        $cultLogo.addClass('glow');
+        setTimeout(function () {
+            $cultLogo.removeClass('glow');
+        }, 500);
+});
+
+function validateForm() {
+    var fName = document.forms["myForm"]["firstname"].value;
+    var lName = document.forms["myForm"]["lastname"].value;
+    var email = document.forms["myForm"]["email"].value;
+    var cEmail = document.forms["myForm"]["confirmemail"].value;
+    if (fName == null || fName == "") {
+        alert("First Name must be filled out");
+        return false;
+    }
+    else if (lName == null || lName == "") {
+        alert("Last Name must be filled out");
+        return false;
+    }
+    else if (email == null || email == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    else if (cEmail == null || cEmail == "" ) {
+        alert("Confirm Email must be filled out");
+        return false;
+    }
+    else if (cEmail != email){
+      alert("Confirm Email and Email must match");
+      return false;
+    }
+}
